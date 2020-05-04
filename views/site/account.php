adedcba313654ebfbd94ba066813ca84b869aed8
Luis Ribeiro <lribeiro@linkya.pt>
2020-05-04 22:07:03 +0100

    Update account page

--- views/site/account.php
+++ views/site/account.php
@@ -22,9 +22,9 @@ $this->params['modals'][] = "apiKey";
     </div>
     <div class="container">
         <div class="mt-5">
-            <div class="px-4">
+            <div class="">
                 <div class="row">
-                    <div class="col-xs-12 col-lg-6 px-lg-5 mb-5">
+                    <div class="col-xs-12 col-lg-5 mb-5">
                         <h5 class="mb-5"><?= Yii::t('app', 'Dados pesssoais') ?></h5>
                         <?php
                         $form = ActiveForm::begin([
@@ -61,7 +61,7 @@ $this->params['modals'][] = "apiKey";
                     </div>
 
                     <?php ActiveForm::end(); ?>
-                    <div class="col-xs-12 col-lg-6 px-lg-5 mb-5">
+                    <div class="col-xs-12 col-lg-5 offset-lg-2 mb-5">
                         <h5 class="mb-5"><?= Yii::t('app', 'Alterar Password') ?></h5>
                         <?php
                         $form2 = ActiveForm::begin([
@@ -91,12 +91,14 @@ $this->params['modals'][] = "apiKey";
                 </div>
             </div>
 
-            <div class="px-4">
+            <div class="">
                 <div class="row ">
-                    <div class="col-xs-12 col-lg-12  px-lg-5 mb-5 mt-5">
+                    <div class="col-xs-12 col-lg-12 mb-5 mt-5">
                         <h5 class="mb-5"><?= Yii::t('app', 'API') ?></h5>
                         <p>
                             <?= Yii::t('app', 'Além deste site, também pode usar a API para criar e apagar links curtos. Se não sabe como funciona uma API, não a use. NÃO partilhe essa chave no lado do cliente do seu site.') ?>
+                            <?= Yii::t('app', 'Tem duvidas?') ?> <?= Html::tag('span', Html::a(Yii::t('app', 'Veja a documentação'), 'api-v1')); ?>.
+
                         </p>
 
                         <p class="mt-4">
@@ -113,17 +115,21 @@ $this->params['modals'][] = "apiKey";
 
                 </div>
             </div>
+            <div class="">
+                <div class="row ">
+                    <div class="col-xs-12 col-lg-12 mb-5 mt-5">
+                        <h5 class="mb-5 text-danger"><?= Yii::t('app', 'Apagar Conta') ?></h5>
+                        <p class="text-danger"> 
+                            <?= Yii::t('app', 'Esta ação vai apagar todos os seus dados e links registados.') ?>
+                            <?= Yii::t('app', 'Depois de ativar esta opção a mesma não pode ser revertida nem os seus dados podem ser recuperados.') ?>
+                            <?= Yii::t('app', 'Continue apenas se tem a certeza que é isso que quer fazer.') ?>
 
-            <div class="px-5">
-                <div class="row border-top">
-                    <div class="col-xs-12 col-lg-6 text-center text-lg-left">
-                        <h5 class="mb-5 mt-5"><?= Yii::t('app', 'Apagar Conta') ?></h5>
+                        </p>
+                        <p class="mt-5">
+                            <?= Html::tag('span', Html::a('<i class="fas fa-trash"></i> '.Yii::t('app', 'Apagar'), '#', ['class' => 'btn btn-danger shadow border-0', 'data-toggle' => 'modal', 'data-target' => '#delete_modal'])); ?>
+                        </p>
                     </div>
-                    <div class="col-xs-12 col-lg-6 text-center text-lg-right">
-                        <div class="btn-wrapper mb-5 mt-5">
-                            <?= Html::tag('span', Html::a('<i class="fas fa-trash"></i> '.Yii::t('app', 'Apagar conta'), '#', ['class' => 'btn btn-danger shadow border-0', 'data-toggle' => 'modal', 'data-target' => '#delete_modal'])); ?>
-                        </div>
-                    </div>
+
                 </div>
             </div>
         </div>
